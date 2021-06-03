f335e27c39a48adf35874859b61b96c845302f11
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2021-06-02 21:22:48 -0500

    add error handling for ratelimiting

--- src/utils/queryErrorHandler.ts
+++ src/utils/queryErrorHandler.ts
@@ -1,10 +1,21 @@
 import { toast } from '../components/Toaster/ToastService';
 import { DataError } from './fetcher';
+import prettyMs from 'pretty-ms';
 
 export function queryErrorHandler(e: DataError) {
-  if (e.code === 401 || e.code === 403) {
-    toast.error('Not Authorized');
-  } else {
-    toast.error('Something went wrong. Please try again later');
+  switch (e.code) {
+    case 401:
+    case 403:
+      toast.error('Not Authorized');
+      break;
+    case 429:
+      toast.error(
+        `Too Many Requests. Please wait ${prettyMs((e.retryAfter || 0) * 1000, {
+          compact: true,
+        })} and try again`,
+      );
+      break;
+    default:
+      toast.error('Something went wrong. Please try again later');
   }
 }
